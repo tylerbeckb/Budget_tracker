0a77308e66dd18ecd493ec23dc92019205cbb269
tylerbeckb <tylerbbrown@icloud.com>
2023-10-10 16:57:38 +0100

    feat: displays page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from app import app
 from flask import render_template
+from app import app
 
 @app.route('/')
 def index():
-    return "Hello World!!!"
+    return render_template('index.html', title='Home')
+
