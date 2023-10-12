8a028ba011fdb04fb1b8dacc94a014bfc00e1457
tylerbeckb <tylerbbrown@icloud.com>
2023-10-12 15:36:25 +0100

    feat: displays income page

--- app/views.py
+++ app/views.py
@@ -3,5 +3,8 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Home')
+    return render_template("index.html", title='Home')
 
+@app.route('/incomeExpeniture')
+def incomeEx():
+    return render_template('incomeEx.html', title='Income & Expentitures')
