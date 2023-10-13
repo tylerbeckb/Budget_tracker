8efe2cda8994512fef89133903532b79c18645ba
tylerbeckb <tylerbbrown@icloud.com>
2023-10-13 14:26:14 +0100

    feat:validation for income form

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import DecimalField
-from wtforms.validators import DataRequired
+from wtforms import DecimalField, SubmitField
+from wtforms.validators import DataRequired, ValidationError, NumberRange
+
 
 class IncomeForm(FlaskForm):
-    number1 = DecimalField('number1', places=2, rounding=None, validators=[DataRequired()])
+    number1 = DecimalField('number1', places=2, rounding=None, 
+                           validators=[DataRequired(), NumberRange(min=0, max=None, message='Input must be positive')])
+    submit = SubmitField('Submit')
+    
+ 
