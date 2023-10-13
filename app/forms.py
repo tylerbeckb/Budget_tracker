6c6a2529d3f499acd50048d1286abb32c5bf332a
tylerbeckb <tylerbbrown@icloud.com>
2023-10-13 15:25:11 +0100

    feat: expenditure form

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import DecimalField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, NumberRange
+from wtforms import DecimalField, SubmitField, StringField
+from wtforms.validators import DataRequired, NumberRange
 
 
 class IncomeForm(FlaskForm):
-    number1 = DecimalField('number1', places=2, rounding=None, 
+    name = StringField('Name')
+    amount = DecimalField('Amount', places=2, rounding=None, 
                            validators=[DataRequired(), NumberRange(min=0, max=None, message='Input must be positive')])
     submit = SubmitField('Submit')
+
+class ExpenditureForm(FlaskForm):
+    name = StringField('Name')
+    amount = DecimalField('Amount', places=2, rounding=None, 
+                           validators=[DataRequired(), NumberRange(min=0, max=None, message='Input must be positive')])
+    submit = SubmitField('Submit')
+
     
  
--- app/views.py
+++ app/views.py
@@ -1,14 +1,16 @@
 from flask import render_template
 from app import app
-from .forms import IncomeForm
+from .forms import IncomeForm, ExpenditureForm
 
 @app.route('/')
 def index():
     return render_template("index.html", title='Home')
 
-@app.route('/incomeExpeniture', methods=['GET', 'POST'])
+@app.route('/incomeExpenditure', methods=['GET', 'POST'])
 def incomeEx():
-    form = IncomeForm()
+    inForm = IncomeForm()
+    exForm = ExpenditureForm()
     return render_template('incomeEx.html', 
                            title='Income & Expentitures', 
-                           form=form)
+                           inForm=inForm,
+                           exForm=exForm)
