8bc12fe5f0c9ab08c212a14f4bea8d23001c7fbe
tylerbeckb <tylerbbrown@icloud.com>
2023-10-13 16:04:59 +0100

    inForm commits to db

--- app/forms.py
+++ app/forms.py
@@ -4,16 +4,15 @@ from wtforms.validators import DataRequired, NumberRange
 
 
 class IncomeForm(FlaskForm):
-    name = StringField('Name')
-    amount = DecimalField('Amount', places=2, rounding=None, 
+    name1 = StringField('Name1')
+    amount1 = DecimalField('Amount1', places=2, rounding=None, 
                            validators=[DataRequired(), NumberRange(min=0, max=None, message='Input must be positive')])
-    submit = SubmitField('Submit')
 
 class ExpenditureForm(FlaskForm):
-    name = StringField('Name')
-    amount = DecimalField('Amount', places=2, rounding=None, 
+    name2 = StringField('Name2')
+    amount2 = DecimalField('Amount2', places=2, rounding=None, 
                            validators=[DataRequired(), NumberRange(min=0, max=None, message='Input must be positive')])
-    submit = SubmitField('Submit')
+
 
     
  
--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from flask import render_template
-from app import app
+from app.models import Incomes
+from flask import render_template, request
+from app import app, db
 from .forms import IncomeForm, ExpenditureForm
+
 
 @app.route('/')
 def index():
@@ -10,6 +12,12 @@ def index():
 def incomeEx():
     inForm = IncomeForm()
     exForm = ExpenditureForm()
+    if inForm.validate_on_submit():
+        name1 = request.form['name1']
+        amount1 = request.form['amount1']
+        record = Incomes(name=name1, amount=amount1)
+        db.session.add(record)
+        db.session.commit()
     return render_template('incomeEx.html', 
                            title='Income & Expentitures', 
                            inForm=inForm,
