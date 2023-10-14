784fbd735454a5de669e1d291e2137257c7bb6fc
tylerbeckb <tylerbbrown@icloud.com>
2023-10-14 15:02:23 +0100

    feat: expenditure write to db and displays the data

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from app.models import Incomes
+from app.models import Incomes, Expenditures
 from flask import render_template, request
 from app import app, db
 from .forms import IncomeForm, ExpenditureForm
@@ -14,15 +14,26 @@ def incomeEx():
     exForm = ExpenditureForm()
 
     incomes = Incomes.query.all() 
+    expenditures = Expenditures.query.all()
 
+    # Writes income data to Incomes db
     if inForm.validate_on_submit():
         name1 = request.form['name1']
         amount1 = request.form['amount1']
         record = Incomes(name=name1, amount=amount1)
         db.session.add(record)
         db.session.commit()
+    # Writes expenditure data to Expenditures db
+    if exForm.validate_on_submit():
+        name2 = request.form['name2']
+        amount2 = request.form['amount2']
+        record = Expenditures(name=name2, amount=amount2)
+        db.session.add(record)
+        db.session.commit()
+    # Renders Page
     return render_template('incomeEx.html', 
                            title='Income & Expentitures', 
                            inForm=inForm,
                            exForm=exForm,
-                           incomes=incomes)
+                           incomes=incomes,
+                           expenditures=expenditures)
