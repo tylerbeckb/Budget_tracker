4ef9a774d955e31459f814609be2685f8ecd6cec
tylerbeckb <tylerbbrown@icloud.com>
2023-10-14 15:23:15 +0100

    fix: page reloads so db can update after every submission

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app.models import Incomes, Expenditures
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from app import app, db
 from .forms import IncomeForm, ExpenditureForm
 
@@ -23,6 +23,7 @@ def incomeEx():
         record = Incomes(name=name1, amount=amount1)
         db.session.add(record)
         db.session.commit()
+        return redirect(url_for('incomeEx'))
     # Writes expenditure data to Expenditures db
     if exForm.validate_on_submit():
         name2 = request.form['name2']
@@ -30,6 +31,7 @@ def incomeEx():
         record = Expenditures(name=name2, amount=amount2)
         db.session.add(record)
         db.session.commit()
+        return redirect(url_for('incomeEx'))
     # Renders Page
     return render_template('incomeEx.html', 
                            title='Income & Expentitures', 
