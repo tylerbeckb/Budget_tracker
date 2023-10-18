82628afac983407fabe735f9b3f1d8d9000543d9
tylerbeckb <tylerbbrown@icloud.com>
2023-10-18 15:12:41 +0100

    feat: forms slidedown when pressed

--- app/static/javascript.js
+++ app/static/javascript.js
@@ -1,8 +1,20 @@
 function inExPage() {
     $(document).ready(function() {
-        $(".inForm").slideUp();
-        $(".exForm").slideUp();
-        $(".inEditForm").slideUp();
-        $(".exEditForm").slideUp();
+        $("#inForm").slideUp();
+        $("#exForm").slideUp();
+        $("#inEditForm").slideUp();
+        $("#exEditForm").slideUp();
+        $(".inForm").click(function() {
+            $("#inForm").slideDown();
+        })
+        $(".exForm").click(function() {
+            $("#exForm").slideDown();
+        })
+        $(".inEditForm").click(function() {
+            $("#inEditForm").slideDown();
+        })
+        $(".exEditForm").click(function() {
+            $("#exEditForm").slideDown();
+        })
     })
 }
