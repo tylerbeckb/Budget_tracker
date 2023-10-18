762dccf403b2922e673a0455fb81a7e3c02f2a36
tylerbeckb <tylerbbrown@icloud.com>
2023-10-18 15:06:46 +0100

    feat: slides forms up

---
+++ app/static/javascript.js
@@ -0,0 +1,8 @@
+function inExPage() {
+    $(document).ready(function() {
+        $(".inForm").slideUp();
+        $(".exForm").slideUp();
+        $(".inEditForm").slideUp();
+        $(".exEditForm").slideUp();
+    })
+}
