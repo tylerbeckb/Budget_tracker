2b5566b3d5765da40238bad9365acf5f68e7282f
tylerbeckb <tylerbbrown@icloud.com>
2023-10-20 10:36:45 +0100

    feat: style and hides add form

--- app/static/javascript.js
+++ app/static/javascript.js
@@ -18,3 +18,12 @@ function inExPage() {
         })
     })
 }
+
+function goalPage() {
+    $(document).ready(function() {
+        $("#GoalForm").hide();
+        $("#addBtn").click(function() {
+            $("#GoalForm").slideDown();
+        })
+    })
+}
