480c19c1e95f9548ad68a4a8d8f2d6a3ecd505bb
tylerbeckb <tylerbbrown@icloud.com>
2023-11-02 11:08:24 +0000

    fix:pep8

--- app/forms.py
+++ app/forms.py
@@ -5,79 +5,89 @@ from app.models import Incomes, Expenditures, Goals
 
 # Add Income Form
 class IncomeForm(FlaskForm):
-    name1 = StringField('Name1', validators=[DataRequired(), Length(min=0, max=20)])
-    amount1 = DecimalField('Amount1', places=2, rounding=None, 
-                           validators=[DataRequired(), NumberRange(min=0, max=10000000)])
+    name1 = StringField('Name1', validators = [DataRequired(), 
+                                                Length(min = 0, max = 20)])
+    amount1 = DecimalField('Amount1', places = 2, rounding = None, 
+                            validators = [DataRequired(), 
+                                NumberRange(min = 0, max = 10000000)])
     submit = SubmitField('Submit')    
 
-    # Validates if name does not exist
+
     def validate_name1(form, field):
-        exists = Incomes.query.filter_by(name=field.data).first()
+        exists = Incomes.query.filter_by(name = field.data).first()
         if exists != None:
             raise ValidationError()
 
 # Add Expenditure form
 class ExpenditureForm(FlaskForm):
-    name2 = StringField('Name2', validators=[DataRequired(), Length(min=0, max=20)])
-    amount2 = DecimalField('Amount2', places=2, rounding=None, 
-                           validators=[DataRequired(), NumberRange(min=0, max=10000000)])
+    name2 = StringField('Name2', validators = [DataRequired(), 
+                                                Length(min = 0, max = 20)])
+    amount2 = DecimalField('Amount2', places = 2, rounding = None, 
+                            validators = [DataRequired(), 
+                                NumberRange(min = 0, max = 10000000)])
     submit3 = SubmitField('Submit')
-    
-    # Validates if name does not exist
+
+
     def validate_name2(form, field):
-        exists = Expenditures.query.filter_by(name=field.data).first()
+        exists = Expenditures.query.filter_by(name = field.data).first()
         if exists != None:
             raise ValidationError()
 
 # Edit Income Form
 class EditIn(FlaskForm) :
-    editName = StringField('editName', validators=[DataRequired(), Length(min=0, max=20)])
-    newName = StringField('newName', validators=[DataRequired()])
-    newAmount = DecimalField('newAmount', places=2, rounding=None, 
-                           validators=[DataRequired(), NumberRange(min=0, max=10000000)])
+    editName = StringField('editName', validators = [DataRequired(), 
+                                                    Length(min = 0, max = 20)])
+    newName = StringField('newName', validators = [DataRequired()])
+    newAmount = DecimalField('newAmount', places = 2, rounding = None, 
+                            validators = [DataRequired(), 
+                                NumberRange(min = 0, max = 10000000)])
     submit2 = SubmitField('Submit') 
 
-    # Validates if the new name does not exist
+
     def validate_newName(self, field):
-        exists = Incomes.query.filter_by(name=field.data).first()
+        exists = Incomes.query.filter_by(name = field.data).first()
         if field.data != self.editName.data:
             if exists != None:
                 raise ValidationError()
 
 # Edit Expenditure Form
 class EditEx(FlaskForm) :
-    editName2 = StringField('editName2', validators=[DataRequired(), Length(min=0, max=20)])
-    newName2 = StringField('newName2', validators=[DataRequired()])
-    newAmount2 = DecimalField('newAmount2', places=2, rounding=None, 
-                           validators=[DataRequired(), NumberRange(min=0, max=10000000)])
+    editName2 = StringField('editName2', validators = [DataRequired(), 
+                                                    Length(min = 0, max = 20)])
+    newName2 = StringField('newName2', validators = [DataRequired()])
+    newAmount2 = DecimalField('newAmount2', places = 2, rounding = None, 
+                            validators = [DataRequired(), 
+                                NumberRange(min = 0, max = 10000000)])
     submit4 = SubmitField('Submit')
 
-    # Validates that new name does not exist
+
     def validate_newName2(self, field):
-        exists = Expenditures.query.filter_by(name=field.data).first()
+        exists = Expenditures.query.filter_by(name = field.data).first()
         if field.data != self.editName2.data:
             if exists != None:
                 raise ValidationError()
-    
+
 # Add Goal Form
-class addGoal(FlaskForm) :
+class AddGoal(FlaskForm) :
     goalName = StringField('goalName')
-    goalAmount = DecimalField('goalAmount', places=2, rounding=None, 
-                              validators=[DataRequired(), NumberRange(min=0, max=10000000)])
-    
-    # Validate there is not a goal
+    goalAmount = DecimalField('goalAmount', places = 2, rounding = None, 
+                                validators = [DataRequired(), 
+                                    NumberRange(min = 0, max = 10000000)])
+
+
     def validate_goalAmount(form, field):
         exists = Goals.query.filter_by().first()
         if exists != None:
             raise ValidationError()
 
 # Edit Goal Form  
-class editGoal(FlaskForm) :
+class EditGoal(FlaskForm) :
     editGoal = StringField('editGoal')
-    editGoalAmount = DecimalField('editGoalAmount', places=2, rounding=None,
-                                  validators=[DataRequired(), NumberRange(min=0, max=10000000)])
-    
-    # Validates goal does not exist
+    editGoalAmount = DecimalField('editGoalAmount', places = 2, rounding = None,
+                                    validators = [DataRequired(), 
+                                        NumberRange(min = 0, max = 10000000)])
+
+
     def validate_editGoalAmount(form, field):
         exists = Goals.query.filter_by().first()
         if exists == None:
--- app/models.py
+++ app/models.py
@@ -1,14 +1,16 @@
 from app import db
 
 class Incomes(db.Model):
-    name = db.Column(db.String(500), index=True, unique=True, primary_key=True)
+    name = db.Column(db.String(500), index = True, unique = True, 
+                    primary_key = True)
     amount = db.Column(db.Float)
 
 class Expenditures(db.Model):
-    name = db.Column(db.String(500), index=True, unique=True, primary_key=True)
+    name = db.Column(db.String(500), index = True, unique = True, 
+                    primary_key = True)
     amount = db.Column(db.Float)
 
 class Goals(db.Model):
-    name = db.Column(db.String(500), index=True)
-    amount = db.Column(db.Float, primary_key=True)
+    name = db.Column(db.String(500), index = True)
+    amount = db.Column(db.Float, primary_key = True)
    
